3617676bb42f15d1d6890511588ccded23de21d7
johan12sc <johansc3612@gmail.com>
2026-04-13 10:28:27 +0000

    clean project1

--- service_ia/grok_client.py
+++ service_ia/grok_client.py
@@ -9,7 +9,7 @@ import os
 # ============================================
 # CONFIGURATION GROQ
 # ============================================
-api_key = os.getenv("GROQ_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 
 GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions"
